78595be03ef4b231da3523452c97b27a285dc743
jaiproy <jaiproy@yahoo.co.in>
2018-07-30 17:43:10 +0530

    Aws Web try

---
+++ src/main/java/com/aws/jenkin/FirstWebDeploy/FirstWebDeployApplication.java
@@ -0,0 +1,12 @@
+package com.aws.jenkin.FirstWebDeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebDeployApplication.class, args);
+	}
+}
